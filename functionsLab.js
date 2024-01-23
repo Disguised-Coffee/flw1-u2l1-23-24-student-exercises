8fb1688828ec444f53ffffa03a34abd45e6166d2
Disguised-Coffee <68920714+Disguised-Coffee@users.noreply.github.com>
2024-01-23 23:58:01 +0000

    Hopefully this works?

--- functionsLab.js
+++ functionsLab.js
@@ -1,24 +1,40 @@
 // 1. Write a function that:
 //   - Console.logs 'Hello World'
+function sop(){
+  console.log('Hello World');
+}
 
 // 2. Write a function with 1 parameter that:
 //  - Console.logs whatever you want it to say.
 //  - This would be a good time to use string interpolation!
+function sop2(whatYouWantToSay){
+  console.log(whatYouWantToSay);
+}
 
 // 3. Write a function with 1 parameter that:
 //  - Prints a random number between 1 and whatever number is for the provided argument. 
 //  - You might need to search how to generate a random number.
 //  - You might need to search how to round a number up.
+function randInt(max){
+  max -= 1;
+  return Math.round(Math.random() * max) + 1;
+}
 
 // 4. Write a function with 3 parameters that:
 //  - Takes the product of the first two parameters and raises them to the power of the third parameter.
 //  - Returns the solution.
 //  - Example: Passing this function 1, 2, 3 should give you back the answer to (1 * 2)^3, which is 8. 
 //  - You may need to search how to raise the power of a number!
+function multiplyAndPow(num1, num2, num3){
+  return Math.pow(num1 * num2, num3);
+}
 
 // 5. Write a function with 1 parameter that:
 //  - Converts Celsius to Fahrenheit.
 //  - The formula to convert Celsius to Fahrenhiet: F = (C * 1.8) + 32
+function convertToFarenheit(celsius){
+  return (celsius * 1.8) + 32;
+}
 
 // 6. Write a function with 1 parameter that:
 //  - Takes a word and consoles the letters in alphabetical order.
@@ -27,15 +43,34 @@
 // BONUS FUNCTIONS!
 // 7. Write a function that:
 //  - Reverses a number.
+function reverseNum(num){
+  return num.toString().split('').reverse().join('').toInt();
+}
 
 // 8. Write a function that:
-//  - If the number is even, prints “This number is EVEN!”
-//  - If the number is odd, prints “This number is ODD!”
+//  - If the number is even, prints "This number is EVEN!"
+//  - If the number is odd, prints "This number is ODD!"
+function evenOrOdd(num){
+  if(num % 2 == 0){
+    console.log('This number is EVEN!');
+  }
+  console.log('This number is ODD!');
+}
 
 // 9. Write a function that:
 //  - Prints every number between 1 and 100.
 //  - This requires a loop - you may need to look this up!
+function printOnetoHundred(){
+  for(let i = 1; i <= 100; i++){
+    console.log(i);
+  }
+}
 
 // 10. Write a function that:
 //  -  Prints every EVEN number between 1 and 100.
 //  - This requires a loop - you may need to look this up!
+function printEvenOnetoHundredButOnlyEvens(){
+  for(let i = 2; i<=100; i+=2){
+    consle.log(i);
+  }
+}
--- functionsPractice.js
+++ functionsPractice.js
@@ -4,24 +4,40 @@
 //   - Multiplies them together, and 
 //   - Logs them to the console.
 
+  
+
+
 // 1. DECLARE & NAME THE FUNCTION
 //   - What should I name this function?
+function multiply
 
 // 2. PARAMETERS
 //   - How many parameters will I need?
 //   - How do I know this?
 //   - What should I call them?
+(num1, num2)
 
 // 3. BODY
 //   - What needs to happen to my parameters to give me the result I want?
 //   - Where do I want the result to show up?
+{
+  return num1 * num2;
+}
 
 // 4. FUNCTION CALL
 //   - Call the function and pass through arguments to see if the function works.
-
-
+console.log(multiply(1205921085214, 124));
 
 // CODE SOLO
 // 5. Write a function that will do the following:
 //   - If the correct password “October” is entered, print the message “Success!” to the console
 //   - Else, print “Try again!” if any other password is entered.
+
+function getPassword(password){
+  if(password === "October"){
+    console.log("Success!");
+  } 
+  else{
+    console.log("Try again!");
+  }
+}
